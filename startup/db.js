b69fb19bc9bde78af6ca82bf6af357b56bc04e2a
Joseph Greene <josephadamgreene@gmail.com>
2022-02-21 22:06:41 -0500

    move DB URI to ENV

--- startup/db.js
+++ startup/db.js
@@ -1,7 +1,7 @@
+require( 'dotenv' ).config();
 const mongoose = require('mongoose');
 
 module.exports = function() {
-  const db = "mongodb+srv://mrjag:3DUc32qfSeMAbaTN@maincluster.aypug.mongodb.net/test?retryWrites=true&w=majority"
-  mongoose.connect(db, {useNewUrlParser: true, useUnifiedTopology: true})
+  mongoose.connect(process.env.DB_URI, {useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => console.log(`Connected to database...`));
 }
