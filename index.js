beb7e5ebb366cd466fa17d895b2a7909fa4a5161
Joseph Greene <josephadamgreene@gmail.com>
2022-02-22 02:56:42 -0500

    Run static React during production, CORS during Development

--- index.js
+++ index.js
@@ -5,11 +5,16 @@ const path = require("path");
 require("./startup/db")();
 require("./startup/routes")(app);
 
-app.use(express.static(path.join(__dirname, "client", "dist")))
+// Because the server-side render of a React application requires the project to be 
+// compiled into a built (dist) folder, which wastes time after every change in development,
+// we will only do so while in PRODUCTION mode.
+if(process.env.MODE === "PRODUCTION") {
+  app.use(express.static(path.join(__dirname, "client", "dist")))
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
-});
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+  });
+}
 
 const port = (process.env.PORT || 5000);
 
--- startup/routes.js
+++ startup/routes.js
@@ -1,7 +1,15 @@
 const express = require('express');
+const cors = require('cors');
 const tests = require('../routes/tests');
 
 module.exports = function(app) {
+  // CORS is only necessary while in DEVELOPMENT mode
+  if (process.env.MODE !== "PRODUCTION") {
+    app.use(cors({
+      origin: ['http://localhost:3000'],
+      credentials: true,
+    }));
+  }
   app.use(express.json());
   app.use('/api/test',  tests);
 }
